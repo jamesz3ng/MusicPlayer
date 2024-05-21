ee1f7c44a2dad1dd42ccf8327e0544f1a462aa49
jamesz3ng <jameszeng23@gmail.com>
2024-05-21 14:44:30 +0900

    renamed assets

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ root = Tk()
 root.title("Music Player")
 root.geometry("500x600")
 
+pygame.mixer.init()
+
+songlist = Listbox(root, bg = "black", fg = "white", width = 100, height = 15)
+songlist.pack()
 
 root.mainloop()
+
