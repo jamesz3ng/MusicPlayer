f3696eb1003dc1b8be6b6f35dc93735ca10c9bce
jamesz3ng <jameszeng23@gmail.com>
2024-05-21 16:52:34 +0900

    feat: add control buttons horizontally

--- main.py
+++ main.py
@@ -1,44 +1,36 @@
-from tkinter import *
+import tkinter as tk
 from PIL import Image, ImageTk
 import pygame
 import os
 
-
-root = Tk()
+root = tk.Tk()
 root.title("Music Player")
 root.geometry("500x600")
 
 pygame.mixer.init()
 
-songlist = Listbox(root, bg = "black", fg = "white", width = 100, height = 15)
+songlist = tk.Listbox(root, bg = "black", fg = "white", width = 100, height = 15)
 songlist.pack()
 
-
-imageAssets = ["backward_button.png", "forward_button.png", "pause_button.png", "play_button.png"]
+imageAssets = ["backward_button.png", "play_button.png", "pause_button.png", "forward_button.png"]
 images = {}
+
+controlFrame = tk.Frame(root)
+controlFrame.pack()
 
 for imageFile in imageAssets:
     # print(f"Assets/{imageFile}")
     pilImage = Image.open(f"Assets/{imageFile}")
+    images[imageFile] = ImageTk.PhotoImage(pilImage)
 
 controlButtons = []
-# for imageFile in imageAssets:
-#     button = tk.
+for i, imageFile in enumerate(imageAssets):
+    button = tk.Button(controlFrame, image = images[imageFile], borderwidth=0)
+    button.grid(row = 0, column = i)
+    controlButtons.append(button)
 
-
-
-pilPlayButton = Image.open("Assets/play_button.png")
-playButtonImage = ImageTk.PhotoImage(pilPlayButton)
-
-
-
-
-
-
-
-playButtonLabel = Label(root, image = playButtonImage)
-playButtonLabel.pack()
-
+# playButtonLabel = tk.Label(root, image = playButtonImage)
+# playButtonLabel.pack()
 
 
 root.mainloop()
