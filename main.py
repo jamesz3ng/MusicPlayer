e07f495be7cbe5e2477298de82aac233306d6d59
jamesz3ng <jameszeng23@gmail.com>
2024-05-21 15:37:29 +0900

    feat: processed button images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 import pygame
 import os
 
@@ -11,6 +12,34 @@ pygame.mixer.init()
 
 songlist = Listbox(root, bg = "black", fg = "white", width = 100, height = 15)
 songlist.pack()
+
+
+imageAssets = ["backward_button.png", "forward_button.png", "pause_button.png", "play_button.png"]
+images = {}
+
+for imageFile in imageAssets:
+    # print(f"Assets/{imageFile}")
+    pilImage = Image.open(f"Assets/{imageFile}")
+
+controlButtons = []
+# for imageFile in imageAssets:
+#     button = tk.
+
+
+
+pilPlayButton = Image.open("Assets/play_button.png")
+playButtonImage = ImageTk.PhotoImage(pilPlayButton)
+
+
+
+
+
+
+
+playButtonLabel = Label(root, image = playButtonImage)
+playButtonLabel.pack()
+
+
 
 root.mainloop()
 
