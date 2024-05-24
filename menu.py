c964edf14cf1ef7d4c67083e34b734bc2fa2c358
jamesz3ng <jameszeng23@gmail.com>
2024-05-24 13:56:25 +0900

    feat: add function to create organise menu

---
+++ menu.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import os
+
+def make_menu(root):
+    menubar = tk.Menu(root)
+    root.config(menu = menubar)
+
+    organise_menu = tk.Menu(menubar)
+    menubar.add_cascade(label="Organise", menu=organise_menu)
+    organise_menu.add_command(label = "Select Folder")
